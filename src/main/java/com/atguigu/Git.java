e8db315ea2cdbd9fcafd36c37474ad6c08f66b3c
sunshihao <sunshihao@fjut.com>
2023-02-17 11:52:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/Git.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author ssh
+ * @create 2023-02-17 11:48
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
