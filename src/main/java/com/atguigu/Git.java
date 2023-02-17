3f6d0035bdfe9ee7256628913a55175e7b6e6297
sunshihao <sunshihao@fjut.com>
2023-02-17 12:55:06 +0800

    second commit000

--- src/main/java/com/atguigu/Git.java
+++ src/main/java/com/atguigu/Git.java
@@ -8,5 +8,10 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
